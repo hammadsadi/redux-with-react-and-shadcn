6ee6acb36d1d9d6cfdcd8bf0f25d580df03ea84d
hammadsadi <hammad.sadi@yahoo.com>
2025-01-03 23:48:46 +0600

    Added Shadcn Form and Modal

--- src/components/customComponents/TaskCard/TaskCard.tsx
+++ src/components/customComponents/TaskCard/TaskCard.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { FaTrashAlt } from "react-icons/fa";
-import { CiSquareCheck } from "react-icons/ci";
 import { TTaskInitType } from "@/types/types";
+import { cn } from "@/lib/utils";
+import { Checkbox } from "@/components/ui/checkbox";
 
 interface ITask {
   task: TTaskInitType;
@@ -12,17 +12,19 @@ const TaskCard = ({ task }: ITask) => {
     <div className="border p-3 rounded">
       <div className="flex justify-between mb-4">
         <div className="flex items-center gap-2">
-          <div className="w-3 h-3 bg-rose-600 rounded-full"></div>
+          <div
+            className={cn("w-3 h-3 rounded-full", {
+              "bg-rose-600": task.priority === "High",
+              "bg-green-600": task.priority === "Low",
+              "bg-yellow-600": task.priority === "Medium",
+            })}
+          ></div>
           <h2>{task?.title}</h2>
         </div>
         {/* Status */}
-        <div className="flex gap-1">
-          <Button size="sm">
-            <FaTrashAlt className="text-rose-700" />
-          </Button>
-          <Button size="sm">
-            <CiSquareCheck className="text-green-700" />
-          </Button>
+        <div className="flex items-center gap-1">
+          <FaTrashAlt className="text-rose-700 cursor-pointer" />
+          <Checkbox />
         </div>
       </div>
       <p>{task?.description}</p>
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/customComponents/TaskCard/task/AddTaskModal";
 import TaskCard from "@/components/customComponents/TaskCard/TaskCard";
 import { taskSelector } from "@/redux/features/task/taskSlice";
 import { useAppSelectopr } from "@/redux/hooks";
@@ -7,6 +8,10 @@ const Tasks = () => {
   console.log(tasks);
   return (
     <div className="max-w-4xl mx-auto">
+      <div className="flex justify-between items-center mb-2">
+        <h2 className="text-3xl">Task</h2>
+        <AddTaskModal />
+      </div>
       <div className="space-y-2">
         {tasks?.map((task) => (
           <TaskCard key={task.id} task={task} />
