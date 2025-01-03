58237383800f8a03d8f1920f7777829b343bf7e1
hammadsadi <hammad.sadi@yahoo.com>
2025-01-03 11:33:48 +0600

    Added Dark and Light Mode using Shad cn UI

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,27 +1,13 @@
-import { Link, Outlet } from "react-router";
+import Navbar from "@/components/shared/Navbar";
+import { Outlet } from "react-router";
 
 const Layouts = () => {
   return (
     <div>
-      <div className="flex items-center gap-4">
-        <h2 className="text-4xl font-bold">Todo App</h2>
-        <nav>
-          <ul className="flex items-center gap-3">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/tasks">Tasks</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Outlet/>
+      <Navbar />
+      <Outlet />
     </div>
   );
-}
+};
 
-export default Layouts
+export default Layouts;
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import { RouterProvider } from "react-router";
 import routes from "./routes/routes.tsx";
+import { ThemeProvider } from "./providers/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-     <RouterProvider router={routes}/>
-    </Provider>
+    <ThemeProvider>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>
 );
